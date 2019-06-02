4821461d648f94281d041189b399eaaa9cdccd20
propertyindicators <propertyindicators@gmail.com>
2019-06-02 16:35:55 +0300

    Tweaked Google search, added site map. Fixed, tweaked and refactored: index, about, mylocale etc.

--- js/_static.js
+++ js/_static.js
@@ -1,3 +1,3 @@
-/*! PILab 26-05-2019 (c) 2017 propertyindicators@gmail.com */
+/*! PILab 02-06-2019 (c) 2017 propertyindicators@gmail.com */
 
 function _MyBuildTest(){alert("TEST: Static build")}
--- js/calc.component.js
+++ js/calc.component.js
@@ -1,2 +1,2 @@
-/*! PILab 26-05-2019 (c) 2017 propertyindicators@gmail.com */
+/*! PILab 02-06-2019 (c) 2017 propertyindicators@gmail.com */
 
