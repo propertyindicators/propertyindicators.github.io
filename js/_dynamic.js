7c2e75a1b9dd1ae6eed03374bf33952e71465795
propertyindicators <propertyindicators@gmail.com>
2019-06-11 07:35:24 +0300

    Changed sitemap.xml

--- js/_dynamic.js
+++ js/_dynamic.js
@@ -1,3 +1,3 @@
-/*! PILab 02-06-2019 (c) 2017 propertyindicators@gmail.com */
+/*! PILab 11-06-2019 (c) 2017 propertyindicators@gmail.com */
 
 function _MyBuildTest(){alert("TEST: Dynamic build")}
--- js/calc.component.js
+++ js/calc.component.js
@@ -1,2 +1,2 @@
-/*! PILab 02-06-2019 (c) 2017 propertyindicators@gmail.com */
+/*! PILab 11-06-2019 (c) 2017 propertyindicators@gmail.com */
 
