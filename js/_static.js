48b6fb3cf7f508aefb8139334df8d65708b6e420
propertyindicators <propertyindicators@gmail.com>
2020-04-13 20:14:06 +0300

    Fixed English trends wording

--- js/_static.js
+++ js/_static.js
@@ -1,3 +1,3 @@
-/*! PILab 11-04-2020 (c) 2017 propertyindicators@gmail.com */
+/*! PILab 13-04-2020 (c) 2017 propertyindicators@gmail.com */
 
 function _MyBuildTest(){alert("TEST: Static build")}
--- js/calc.component.js
+++ js/calc.component.js
@@ -1,2 +1,2 @@
-/*! PILab 11-04-2020 (c) 2017 propertyindicators@gmail.com */
+/*! PILab 13-04-2020 (c) 2017 propertyindicators@gmail.com */
 
